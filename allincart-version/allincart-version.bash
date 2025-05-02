06ddf05570a3b1cb27b02db60bac53f6e07aa30f
gxs <lyricsong47@gmail.com>
2025-05-02 20:25:12 +0800

    init

---
+++ allincart-version/allincart-version.bash
@@ -0,0 +1,45 @@
+#!/usr/bin/bash
+
+set -e
+set pipefail
+
+get_ref() {
+    local ref
+
+    if [[ -n "${HEAD_REF}" ]]; then
+        echo "refs/heads/${HEAD_REF}"
+    else
+        echo "${REF}"
+    fi
+
+}
+
+ref="$(get_ref)"
+echo "Local ref: ${ref}"
+
+PR_NR=$(echo "${ref}" | sed -E -n "s|^refs/heads/gh-readonly-queue/[^/]+/pr-([0-9]+)-.*$|\1|p")
+if [[ -n "${PR_NR}" ]]; then
+    echo "Found merge queue ref with PR Nr. ${PR_NR}" 2>&1
+
+    ref="refs/heads/$(gh pr view --repo "${CURRENT_REPO}" "${PR_NR}"  --jq '.headRefName' --json headRefName)"
+    BASE_REF="$(gh pr view --repo "${CURRENT_REPO}" "${PR_NR}"  --jq '.baseRefName' --json baseRefName)"
+fi
+
+remote_ref=$(git ls-remote --heads "https://github.com/${REPO}" "${ref}" | cut -f 2)
+if [[ -n "${remote_ref}" ]]; then
+    version="${remote_ref#"refs/heads/"}"
+else
+    remote_ref=$(git ls-remote --heads "https://github.com/${REPO}" "refs/heads/${BASE_REF}" | cut -f 2)
+    if [[ -n "${remote_ref}" ]]; then
+        version="${remote_ref#"refs/heads/"}"
+    fi
+fi
+
+if [[ -z "$version" ]]; then
+    echo "$ref not found in ${REPO}, using fallback "
+    version="${FALLBACK}"
+fi
+
+echo "Matching allincart version: $version"
+
+echo "allincart-version=$version" >> "$GITHUB_OUTPUT"
